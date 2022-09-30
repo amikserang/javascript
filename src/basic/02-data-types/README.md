2a738feb5d7bc09e97244c6764406ee4728f6005
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-09-30 15:01:05 +0700

    feat: menambahkan data type primitif

---
+++ src/basic/02-data-types/README.md
@@ -0,0 +1,30 @@
+# Tipe Data
+
+```js
+"string";
+24; // number
+false | true;
+null;
+undefined;
+```
+
+tipe data adalah satu set nilai dan satu set operasi yang diizinkan terhadap value tersebut. Tipe data memberi tahu [compiler](https://id.wikipedia.org/wiki/Kompilator) atau [interpreter](<https://id.wikipedia.org/wiki/Penerjemah_(komputasi)>) bagaimana programmer diharuskan menggunakan data tersebut.
+
+## Jenis-Jenis Tipe data
+
+Di JavaScript, tipe data terbagi menjadi dua jenis, yaitu primitive(primitif) dan non-primitive(non-primitif)
+
+### Tipe Data Primitif
+
+1. **string**
+   Tipe data string merupakan tipe data yang diapit menggunakan tanda kutip `( '' )` atau tanda kutip dua `( "" )` tipe data ini bersifat layaknya seperti teks.
+2. **number**
+   Tipe data angka merepresentasikan sebuah angka biasa
+   dari bilangan minus sekalipun hingga positif adalah
+   sebuah tipe data number. Kamu dapat melakukan operasi matematika ketika menggunakan tipe data number.
+3. **boolean**
+   Tipe data boolean adalah tipe data yang hanya berupa dua buah kondisi dimana ada `false` dan `true`, atau dalam bahasa indonesia, `benar` atau `salah`.
+4. **null**
+   Tipe data null sederhananya adalah sebuah representasi data yang ada, namun belum mempunyai value yang jelas, seperti `string`, `angka`, atau `boolean`.
+5. **undefined**
+   Tipe data undefined adalah sebuah tipe data yang bersifat _falsy_, undefined sederhananya memberi tahu komputer bahwa, hal tersebut belum di definisikan dimanapun.
---
+++ src/basic/02-data-types/primitive.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * Tipe data string merupakan tipe data yang diapit menggunakan tanda kutip ( '' ) atau tanda kutip dua ( "" )
+ * tipe data ini bersifat layaknya seperti teks, perlu diperhatikan
+ * ketika kamu menggunakan tipe data string dan mengisi angka didalamnya seperti "12"
+ * namun ketika kamu melakukan operasi matematika seperti "12" + "12", hasilnya akan menjadi "1212"
+ * karena tipe data yang digunakan adalah sebuah string, dan bukan sebuah angka
+ */
+let tipeDataString = "Halo, tipe data ini sebuah string";
+
+/**
+ * Tipe data number merepresentasikan sebuah angka biasa
+ * dari bilangan minus sekalipun hingga positif adalah
+ * sebuah tipe data number atau dalam bahasa Indonesia, angka
+ * kamu dapat melakukan operasi matematika ketika menggunakan tipe data angka
+ */
+let tipeDataAngka = 17;
+
+/**
+ * Tipe data boolean adalah tipe data yang hanya berupa dua buah kondisi
+ * dimana ada false dan true, atau dalam bahasa indonesia, benar atau salah
+ * tipe data ini sederhananya adalah sebuah saklar lampu
+ * yang dapat memadamkan dan menyalakan lampu ruangan
+ */
+let tipeDataBoolean = true; // atau false
+
+/**
+ * null adalah sebuah tipe data yang ada di JavaScript
+ * null, sederhananya merepresentasikan sebuah data yang ada, namun belum mempunyai
+ * isi data yang jelas, seperti string, angka, atau boolean
+ */
+let belumAdaNilai = null;
+
+/**
+ * Undefined adalah sebuah tipe data yang ada di JavaScript, bersifat _falsy_
+ * undefined sederhananya memberi tahu komputer bahwa, hal tersebut belum di definisikan dimanapun
+ */
+let tidakDiDefinisikan = undefined;
