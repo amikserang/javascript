486bb6b80b7dfb05772fe46f1b86fbc2c3a80e05
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-09-30 14:59:03 +0700

    feat: menambahkan commitizen, lint-staged, dan husky

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "none",
+  arrowParens: "always",
+  endOfLine: "auto"
+};
+
+module.exports = config;
